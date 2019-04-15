4331c83bbbcc3441969695e7294d67965fcc62e8
Floki95 <oliverthomas.info@gmail.com>
2019-04-15 13:01:19 +0200

    jeu op

--- MasterMind/ViewController.swift
+++ MasterMind/ViewController.swift
@@ -18,6 +18,9 @@ class ViewController: UIViewController {
     
     @IBOutlet var buttonsMaster: [UIButton]!
     
+    @IBOutlet weak var labelScore: UILabel!
+    
+    
     var buttonBoardIndex = 0
     var turn = 0
     var colorMaster = [#colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1),#colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1),#colorLiteral(red: 0, green: 0.9768045545, blue: 0, alpha: 1),#colorLiteral(red: 0.9994240403, green: 0.9855536819, blue: 0, alpha: 1)]
@@ -52,8 +55,8 @@ class ViewController: UIViewController {
         if titleValueString=="START !"{
             sender.setTitle("GO !",for: UIControlState.normal)
 
-            for nbDeLignes in 4..<buttonsBoard.count {
-                buttonsBoard[nbDeLignes].isEnabled = false
+            for nbDeLignes in 0..<4 {
+                buttonsBoard[nbDeLignes].isEnabled = true
                 //print(nbDeLignes)
             }
             
@@ -61,10 +64,8 @@ class ViewController: UIViewController {
                 let color = [#colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1),#colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1),#colorLiteral(red: 0, green: 0.9768045545, blue: 0, alpha: 1),#colorLiteral(red: 0.9994240403, green: 0.9855536819, blue: 0, alpha: 1),#colorLiteral(red: 0.5791940689, green: 0.1280144453, blue: 0.5726861358, alpha: 1),#colorLiteral(red: 0.6679978967, green: 0.4751212597, blue: 0.2586010993, alpha: 1)]
                 let randomNum =  Int(arc4random_uniform(6))
                 colorMaster[nbDeLignes] = color[randomNum]
-                buttonsMaster[nbDeLignes].backgroundColor = color[randomNum]
+                buttonsMaster[nbDeLignes].backgroundColor = colorMaster[nbDeLignes]
             }
-            
-            
             
         }
         else if turn < 5 {
@@ -72,7 +73,7 @@ class ViewController: UIViewController {
             
             let firstEnable = turn*4
             let lastEnable = turn*4+3
-            
+
             //enable next row of button
             for nbDeLignes in firstEnable...lastEnable {
                 buttonsBoard[nbDeLignes].isEnabled = true
@@ -81,11 +82,10 @@ class ViewController: UIViewController {
         
             let firstDisable = (turn-1)*4
             let lastDisable = (turn-1)*4+3
-     
+            
             //disable last row of button
             for nbDeLignes in firstDisable...lastDisable {
                 buttonsBoard[nbDeLignes].isEnabled = false
-               // print(nbDeLignes)
             }
             
             //set clew
@@ -93,51 +93,69 @@ class ViewController: UIViewController {
             for nbDeLignes in 0..<colorTest.count {
                 colorTest[nbDeLignes] = colorMaster[nbDeLignes]
             }
-            
+ 
             var indexClew = firstDisable
             var indexColorTest = 0
-            
             //on test d'abord s'ils sont a la bonne place
+            
+            var checked = [false,false,false,false]
+            var indexCheck = 0
+            
              for i in firstDisable...lastDisable {
                 if buttonsBoard[i].backgroundColor == colorTest[indexColorTest]{
                     colorTest[indexColorTest]=#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
                     labelPin[indexClew].backgroundColor = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
+                    checked[indexCheck] = true
                     indexClew += 1
+                    print("indexclew \(indexClew)")
                 }
-                
+                indexCheck += 1
                 indexColorTest += 1
             }
-            indexClew = firstDisable
+            
+            indexCheck = 0
+            
             for i in firstDisable...lastDisable {
                 
                 for y in 0...3 {
                     
                     if buttonsBoard[i].backgroundColor == colorTest[y]{
                         colorTest[y]=#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-                        if i == y{
-                          labelPin[indexClew].backgroundColor = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
-                        } else {
+                        if (i != y && checked[indexCheck] == false) {
                             labelPin[indexClew].backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+                            indexClew += 1
+                            //on a trouvé on sort de la boucle
+                            break
                         }
-                        indexClew += 1
-                        //on a trouvé on sort de la boucle
-                       break
                     }
-                    labelPin[y].isEnabled = false
-                    // print(nbDeLignes)
                 }
-                
-                
-                labelPin[i].isEnabled = false
-                // print(nbDeLignes)
+                indexCheck += 1
             }
             
+
+         //   print("indexclew \(indexClew)")
+            
+            var win = true
+            //test si win
+            
+            for i in firstDisable...lastDisable{
+                if labelPin[i].backgroundColor != #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1) {
+                    win = false
+                    break
+                }
+            }
+            
+            if win == true{
+                labelScore.text = "Win, Score : \(turn)"
+                
+                for nbDeLignes in 0..<buttonsMaster.count {
+                    buttonsMaster[nbDeLignes].backgroundColor = colorMaster[nbDeLignes]
+                }
+            }
             
             buttonBoardIndex = firstEnable;
             
         }
-        
-     
         
         
     }
